89e1c07d4423eb913066ef68e66f061af1573cb1
dongleqiang <dlq_tinysoft@163.com>
2018-10-10 07:30:44 +0800

    gradle构建项目小demo

---
+++ src/main/java/com/imooc/gradle/todo/App.java
@@ -0,0 +1,12 @@
+package com.imooc.gradle.todo;
+
+/**
+ * Created by 董乐强 on 2018/10/8.
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+
+}
